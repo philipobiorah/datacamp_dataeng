25deb68295fa7bb9aae0d1563a239105ec34718e
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-11-16 21:08:07 +0000

    single line read pd dot read sql query

--- sqlalchemy.py
+++ sqlalchemy.py
@@ -16,12 +16,16 @@ print(table_names)
 
 #Open engine connection: con
 with engine.connect() as con:
-    rs = con.execute("SELECT * FROM Album")
+    rs = con.execute("SELECT * FROM Employee WHERE EmployeeID >= 6")
 
     #Save the reuslts of the query to DataFrame: df
-    # df = pd.DataFrame(rs.fetchall())
+    df = pd.DataFrame(rs.fetchall())
 
     #retrrive e records 
     df = pd.DataFrame(rs.fetchmany(size=3))
 
 print(df.head())
+
+
+#using  one line of code
+df = pd.read_sql_query("SELECT * FROM Employee WHERE EmployeeID >= 6", engine)
