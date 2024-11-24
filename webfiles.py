f9f3eef3a9ce11c433d4f49bca953803c7340fe7
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-11-24 22:54:55 +0000

    modified webfiles.py

--- webfiles.py
+++ webfiles.py
@@ -20,3 +20,21 @@ print(df.head())
 
 # 0            7.4              0.70         0.00             1.9      0.076
 
+# reading excel web file
+# Assign url of file: url
+url = 'http://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/latitude.xls'
+
+# Read in all sheets of Excel file: xls
+xls = pd.read_excel(url, sheet_name=None)
+
+# Print the sheetnames to the shell
+print(xls.keys())
+#output: dict_keys(['1700', '1900'])
+
+# Print the head of the first sheet (using its name, NOT its index)
+print(xls['1700'].head())
+#output:    country       1700  1701  1702  1703  1704  1705  1706  1707  1708  ...  \  
+# 0  Afghanistan  34.5650   NaN   NaN   NaN   NaN   NaN   NaN   NaN   NaN  ...
+# 1      Albania  41.0000   NaN   NaN   NaN   NaN   NaN   NaN   NaN   NaN  ...
+# 2      Algeria  36.7500   NaN   NaN   NaN   NaN   NaN   NaN   NaN   NaN  ...
+# 3      Andorra  42.5075   NaN   NaN   NaN   NaN   NaN   NaN   NaN   NaN  ...
