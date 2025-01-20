82f70b3f69c0e5225ec6b16e144f0bd64c206b05
PHILIP OBIORAH <philip.obiorah@outlook.com>
2025-01-20 22:55:01 +0000

    assert fullname has no nonorifics

--- data_clean.py
+++ data_clean.py
@@ -118,3 +118,8 @@ airlines['full_name'] = airlines['full_name'].str.replace("Miss", "")
 
 # Replace "Ms." with empty string ""
 airlines['full_name'] = airlines['full_name'].str.replace("Ms.", "")
+
+
+# Assert that full_name has no honorifics
+# Assert that full_name has no honorifics
+assert airlines['full_name'].str.contains('Ms.|Mr.|Miss|Dr.').any() == False
